e7c61f3f0e4ae47eb846e101819aff6a212a514c
PLANETORIAN <daksh06shah2006@gmail.com>
2025-02-05 00:31:28 +0530

    Structure of the webpage

---
+++ script.js
@@ -0,0 +1,46 @@
+const qstns = [
+    {
+    question: "What is the capital city of Australia?",
+    ans: {
+        opt1:"Sydney",
+        opt1:"Melbourne",
+        opt1:"Canberra",
+        opt1:"Brisbane"
+    },
+    correctAns: "Canberra"
+    },
+    {
+        question: "What is the capital city of Australia?",
+        ans: {
+        opt1:"Sydney",
+        opt1:"Melbourne",
+        opt1:"Canberra",
+        opt1:"Brisbane"
+        },
+        correctAns: "Canberra"
+    },
+    {
+    question: "What is the capital city of Australia?",
+    ans: {
+        opt1:"Sydney",
+        opt1:"Melbourne",
+        opt1:"Canberra",
+        opt1:"Brisbane"
+    },
+    correctAns: "Canberra"
+    },
+    {
+    question: "What is the capital city of Australia?",
+    ans: {
+        opt1:"Sydney",
+        opt1:"Melbourne",
+        opt1:"Canberra",
+        opt1:"Brisbane"
+    },
+    correctAns: "Canberra"
+    }
+]
+
+const qstnElement = document.querySelector(".question");
+const ansElement = document.querySelector(".answers");
+const nextElement = document.querySelector(".next-button");
